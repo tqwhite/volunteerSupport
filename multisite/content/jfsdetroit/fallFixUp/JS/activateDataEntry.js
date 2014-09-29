d82014fd8b4892f8341baed005700986a67d544c
TQ White II <tq@justkidding.com>
2014-09-29 00:17:26 -0500

    user info supports: works, saves, auto-creates fields, except for radio buttons

--- multisite/content/jfsdetroit/fallFixUp/JS/activateDataEntry.js
+++ multisite/content/jfsdetroit/fallFixUp/JS/activateDataEntry.js
@@ -7,3 +7,30 @@ parameterFileName:'simpleData.ini'
 
 }
 );
+
+
+if (location.host.match('local')){
+var init={
+	'firstName':'TQ',
+	'lastName':'White',
+	'address':'5004 Three Points Blvd',
+	'city':'Mound',
+	'state':'MN',
+	'zip':'55364',
+	'windowWashingCount':'2',
+	'winterizingWindowsCount':'3',
+	'winterizingWindowsInformation':'wintery windows',
+	'winterizingDoorsCount':'4',
+	'winterizingDoorsInfo':'open those wintery doors',
+	'clientInfo':'crotchety old man',
+	'socialWorker':'Emily White',
+}
+
+	for (var i in init){
+		var element=init[i];
+
+		$("[name='"+i+"']").val(element);
+	}
+
+
+}
